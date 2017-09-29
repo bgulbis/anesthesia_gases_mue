2de5a9579999b4405fa3597443666b54225684bc
bgulbis <brian.gulbis@gmail.com>
2017-09-29 13:55:28 -0500

    udpate query criteria

--- src/01_find-patients.R
+++ src/01_find-patients.R
@@ -14,7 +14,7 @@ pts_mbo <- read_data(dir_raw, "patients", FALSE) %>%
     as.patients() %>%
     filter(age >= 18)
 
-pts_id <- concat_encounters(pts_mbo$millennium.id, 500)
+pts_id <- concat_encounters(pts_mbo$millennium.id, 1000)
 
 # Run MBO query:
 #   * Clinical Events - No Order Id - Prompt
@@ -30,9 +30,8 @@ x <- count(gases, event)
 
 # Run MBO query:
 #   * Clinical Events - No Order Id - Prompt
-#       - Clinical Event: Inspired Desflurane - Anes, Inspired Isoflurane -
-#       Anes, Inspired Sevoflurne - Anes, Expired Desflurane - Anes, Expired
-#       Isoflurane - Anes, Expired Sevoflurne - Anes, N2O - Anes, Fi N2O - Anes,
+#       - Clinical Event: Inspired Desflurane - Anes;Inspired Isoflurane - Anes;
+#           Inspired Sevoflurne - Anes;N2O - Anes;O2 - Anes;Air - Anes
 #       Expired N2O - Anes
 #   * Demographics
 #   * Diagnosis - ICD-9/10-CM
@@ -49,3 +48,8 @@ id_edw <- concat_encounters(pts_edw$pie.id, 1000)
 
 # run EDW query:
 #   * Surgeries
+
+# x <- read_data(dir_raw, "events", FALSE)
+#
+# y <- read_data(dir_raw, "surgeries", FALSE) %>%
+#     distinct()
